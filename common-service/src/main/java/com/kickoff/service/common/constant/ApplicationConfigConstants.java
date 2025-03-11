5afe1c0ef995f6e0a448ba969ccb9acac5695b35
최준혁 <cwnsgur13579@gmail.com>
2025-03-11 10:48:10 +0900

    신규: #3 공통 신규 모듈 생성

---
+++ common-service/src/main/java/com/kickoff/service/common/constant/ApplicationConfigConstants.java
@@ -0,0 +1,18 @@
+package com.kickoff.service.common.constant;
+
+import java.util.List;
+
+public class ApplicationConfigConstants {
+  public static final List<Integer> AVAILABLE_LEAGUE_API_FOOTBALL_LEAGUE_IDS = List.of(
+    //    2  // UEFA
+    39  // EPL
+    , 140  // LaLiga
+    , 135  // Serie A
+    , 78 // Bundesliga
+    , 61 // France Ligue 1
+    , 292 // K League 1
+  );
+
+  private ApplicationConfigConstants() {
+  }
+}
