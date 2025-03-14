62bd843ddd69b22ca2c13785dd8ef316bf4d8bd2
최준혁 <cwnsgur13579@gmail.com>
2025-03-14 10:37:44 +0900

    신규: #6 팀 기초 데이터 동기화

--- team-service/src/main/java/com/kickoff/service/team/domain/service/TeamCommandServiceImpl.java
+++ team-service/src/main/java/com/kickoff/service/team/domain/service/TeamCommandServiceImpl.java
@@ -26,14 +26,6 @@ public class TeamCommandServiceImpl implements TeamCommandService {
   private final TeamExternalApiService teamExternalApiService;
   private final TeamRepository teamRepository;
 
-//  팀에서 리그대상으로할 리그id값들을 리그로보낸다
-//  대상으로 할 리그-연도 값을 반환한다
-//  해당 리그들의 apiId, season 값으로 외부호출한다
-//  받은 팀정보에 해당하는 팀id를 리그모듈에 보낸다.
-//    - 없는팀이면 저장하고 팀id를 보낸다.
-//  팀id 를 해당시즌에 저장한다
-
-  // fetchSeasons -> apiCall(fetchTeams) -> updateSeason
   @Override
   public void initTeamsAndSeasons() {
     ApplicationConfigConstants.AVAILABLE_LEAGUE_API_FOOTBALL_LEAGUE_IDS.stream()
@@ -58,10 +50,5 @@ public class TeamCommandServiceImpl implements TeamCommandService {
       initTeamsCommand.addAllTeams(year, teamIds);
     }
     streamBridge.send("init-season-map-teams", initTeamsCommand);
-    System.out.println();
-    // api쏘고
-    // 없는거 저장하고
-    // command 에 팀id 체우고
-    // 반환
   }
 }
